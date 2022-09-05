0a55ab518293b8f8e7e3757f4ae147ec93444f43
AshfatAbegh <mdabegh50@gmail.com>
2022-09-06 00:59:15 +0600

    Portfolio Done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
               </div>
 
               <p className = "text-xl mt-10">
-                I am pursuing my Bachelor of Science in Computer Science and Engineering (CSE) degree from one of the renowned private university in Bangladesh which is American International University - Bangladesh (AIUB). I started my university in 2019 for the 4 year BSc. program and Currently, I am the final year student. Obtaining necessary skills to start a Software Developer career is quite challenging and time consuming, even for those who genuinely enjoy everything about the process. I have been working on Web Development and Web Application Development area since 2020. Right now, I'm looking for the chance to gain work experience so that I'll improve my skills.
+                I am pursuing my Bachelor of Science in Computer Science and Engineering (CSE) degree from one of the renowned private university in Bangladesh which is American International University - Bangladesh (AIUB). I started my university back in 2019 for the 4 year BSc. program and Currently, I am the final year student. Obtaining necessary skills to start a Software Developer career is quite challenging and time consuming, even for those who genuinely enjoy everything about the process. I have been working on Web Development and Web Application Development area since 2020. I have 2 years experience in making custom Web sites and Web Application. I like Problem Solving and also I can work under pressure. Also I have strong communication skill which helps me to work with teams. Right now, I'm looking for the chance to gain work experience so that I'll improve my skills.
               </p>
             </div>
         </div>
